9a59ed385b773670b1a6d8b8e0b4a897decfbf76
Kirill <litovchenko.br@gmail.com>
2023-04-10 18:42:18 +0300

    hw
    finished all tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
 import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const throttle = require('lodash.throttle');
+
+const STORAGE_VIDEO_CURRENT_TIME = 'videoplayer-current-time';
+
+startVidoePreviosTime();
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+function saveCurrentTime(e) {
+  localStorage.setItem(STORAGE_VIDEO_CURRENT_TIME, JSON.stringify(e));
+}
+
+function startVidoePreviosTime() {
+  const savedTime = localStorage.getItem(STORAGE_VIDEO_CURRENT_TIME);
+  if (savedTime) {
+    const time = JSON.parse(savedTime);
+    player.setCurrentTime(time.seconds);
+  }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
 
+const STORAGE_FEEDBACK_FORM = 'feedback-form-state';
+
+refs.form.addEventListener('input', onFormInput);
+refs.form.addEventListener('submit', onFormSubmit);
+
+let formData = {};
+populateFormInput();
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+
+  localStorage.setItem(STORAGE_FEEDBACK_FORM, JSON.stringify(formData));
+}
+
+function populateFormInput() {
+  const savedInput = localStorage.getItem(STORAGE_FEEDBACK_FORM);
+
+  if (savedInput) {
+    const parsedSavedInput = JSON.parse(savedInput);
+    refs.input.value = parsedSavedInput.email;
+    refs.textarea.value = parsedSavedInput.message;
+  }
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const savedInput = localStorage.getItem(STORAGE_FEEDBACK_FORM);
+  const parsedSavedInput = JSON.parse(savedInput);
+  console.log(parsedSavedInput);
+
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_FEEDBACK_FORM);
+}
